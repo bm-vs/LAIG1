a680d0a1e23903d30c7dc2a4aca0ff1089c6c953
BSantos22 <bruno.santos1996@gmail.com>
2016-09-27 12:05:06 +0100

    Initial code from example

---
+++ reader/MySceneGraph.js
@@ -0,0 +1,65 @@
+
+function MySceneGraph(filename, scene) {
+	this.loadedOk = null;
+	
+	// Establish bidirectional references between scene and graph
+	this.scene = scene;
+	scene.graph=this;
+		
+	// File reading 
+	this.reader = new CGFXMLreader();
+
+	/*
+	 * Read the contents of the xml file, and refer to this class for loading and error handlers.
+	 * After the file is read, the reader calls onXMLReady on this object.
+	 * If any error occurs, the reader calls onXMLError on this object, with an error message
+	 */
+	 
+	this.reader.open('scenes/'+filename, this);  
+}
+
+/*
+ * Callback to be executed after successful reading
+ */
+MySceneGraph.prototype.onXMLReady=function() 
+{
+	console.log("XML Loading finished.");
+	var rootElement = this.reader.xmlDoc.documentElement;
+	
+	// Here should go the calls for different functions to parse the various blocks
+	var error = this.parseDSX(rootElement);
+
+	if (error != null) {
+		this.onXMLError(error);
+		return;
+	}	
+
+	this.loadedOk=true;
+	
+	// As the graph loaded ok, signal the scene so that any additional initialization depending on the graph can take place
+	this.scene.onGraphLoaded();
+};
+
+
+
+/*
+ * Parses elements of one block and stores information in a specific data structure
+ */
+MySceneGraph.prototype.parseDSX= function(rootElement) {
+	
+
+	/* TODO */
+
+
+};
+	
+/*
+ * Callback to be executed on any read error
+ */
+ 
+MySceneGraph.prototype.onXMLError=function (message) {
+	console.error("XML Loading Error: "+message);	
+	this.loadedOk=false;
+};
+
+
